f878d2321ed1503bcd4d0c375c2c05547bb1ccf9
MengCheng1015 <858753441@qq.com>
2020-12-13 12:21:59 +0800

    update

---
+++ JS/search.js
@@ -0,0 +1,1 @@
+;var ru="//auth.icp.bili.bio";function s(){console.log("search request received");var keyword=document.getElementById('keyword').value;window.location.href=ru+"/"+keyword;}
---
+++ JS/stop.js
@@ -0,0 +1,6 @@
+Swal.fire({
+  type: 'error',
+  title: '本站查询功能暂停服务！',
+  text: '我们遇到了一些技术问题，正在努力修复！',
+  footer: '<a href="https://github.com/STARICP/STARICP/">要不先来我们的github仓库点个小star？</a>'
+})
